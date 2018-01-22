6dc741f8c817d3cc603f1c069c6319766394fa1c
Brandon <bvcoll@wpi.edu>
2018-01-22 13:32:06 -0500

    Simplified PID drive movement

--- PID_Drive.c
+++ PID_Drive.c
@@ -24,8 +24,7 @@ float diffI = 0.05;
 float diffD = 0.2;
 
 //////////////////////////////////////////////////////////* DRIVE PID TASK *//////////////////////////////////////////////////////////////////
-// This task relies on executing two loops, one for driving and one for turning. Could definitely be optimized and number of lines reduced, but
-// we're on a time frame. TODO for later.
+
 task PID_Drive(){
 	while(true){
 
@@ -39,10 +38,15 @@ task PID_Drive(){
 		}
 
 		//Runs the PID loop while isDriving is true and sets isDriving to false when done.
-		while(isDriving){
+		while(isDriving || isTurning){
 			// Calculate both linear and difference errors
-			disterror = linearDistance - ((SensorValue(leftEncoder) + SensorValue(rightEncoder))/2); //Calculate distance error
-			differror = SensorValue(leftEncoder) - SensorValue(rightEncoder); //Calculate difference error
+			if(isDriving){
+				disterror = linearDistance - ((SensorValue(leftEncoder) + SensorValue(rightEncoder))/2); //Calculate distance error
+				differror = SensorValue(leftEncoder) - SensorValue(rightEncoder); //Calculate difference error
+				} else if(isTurning){
+				disterror = turnAng*ANG_CONST - ((SensorValue(leftEncoder) + -1*SensorValue(rightEncoder))/2); //Calculate distance error
+				differror = SensorValue(leftEncoder) - -1*SensorValue(rightEncoder); //Calculate difference error
+			}
 
 			// Find the integral ONLY if within controllable range AND if the distance error is not equal to zero
 			if( abs(disterror) < 60 && disterror != 0){
@@ -77,57 +81,14 @@ task PID_Drive(){
 				distspeed = -maxspeed;
 			}
 
-			motor[FLD] = motor[BLD] = distspeed - diffspeed; //Set motor values
-			motor[FRD] = motor[BRD] = distspeed + diffspeed; //Set motor values
-
-			if(disterror<driveErrorThreshold){
-				isDriving = false;
-				linearDistance = 0;
+			if(isDriving){
+				motor[FLD] = motor[BLD] = distspeed - diffspeed; //Set motor values
+				motor[FRD] = motor[BRD] = distspeed + diffspeed; //Set motor values
 			}
-			wait1Msec(20);
-		}
-
-		//Runs the PID loop while isTurning is true and sets isTurning to false when done. Turns to the right by default for positive values.
-		while(isTurning){
-
-			disterror = turnAng*ANG_CONST - ((SensorValue(leftEncoder) + -1*SensorValue(rightEncoder))/2); //Calculate distance error
-			differror = SensorValue(leftEncoder) - -1*SensorValue(rightEncoder); //Calculate difference error
-
-			// Find the integral ONLY if within controllable range AND if the distance error is not equal to zero
-			if( abs(disterror) < 60 && disterror != 0){
-				distintegral = distintegral + disterror;
-				} else {
-				distintegral = 0; //Otherwise, reset the integral
+			else if(isTurning){
+				motor[FLD] = motor[BLD] = distspeed - diffspeed; //Set motor values
+				motor[FRD] = motor[BRD] = -1*(distspeed + diffspeed); //Set motor values
 			}
-
-			// Find the integral ONLY if within controllable range AND if the difference error is not equal to zero
-			if(abs(differror) < 60 && differror != 0){
-				diffintegral = diffintegral + differror;
-				} else {
-				diffintegral = 0; //Otherwise, reset the integral
-			}
-
-			distderivative = disterror - prevdisterror; //Calculate distance derivative
-			diffderivative = differror - prevdifferror; //Calculate difference derivative
-
-			prevdisterror = disterror; //Update previous distance error
-			prevdifferror = differror; //Update previous difference error
-
-			distspeed = (disterror * distP) + (distintegral * distI) + (distderivative * distD); //Calculate distance speed
-			diffspeed = (differror * diffP) + (diffintegral * diffI) + (diffderivative* diffD); //Calculate difference (turn) speed
-
-			//Check that the speed is not exceeding the maximum set speed
-			if(distspeed > maxspeed){
-				distspeed = maxspeed;
-			}
-
-			//Check that the speed is not exceeding the maximum set speed
-			if(distspeed < -maxspeed){
-				distspeed = -maxspeed;
-			}
-
-			motor[FLD] = motor[BLD] = distspeed - diffspeed; //Set motor values
-			motor[FRD] = motor[BRD] = -1*(distspeed + diffspeed); //Set motor values
 
 			if(disterror<turnErrorThreshold){
 				isTurning = false;
@@ -135,8 +96,6 @@ task PID_Drive(){
 			}
 			wait1Msec(20);
 		}
-
-		wait1Msec(20);
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
